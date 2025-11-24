8fe5ee7f763df6c71b7e4d47d4cc457f38d25ab0
Renke Brixel <renke.brixel@gmail.com>
2025-11-24 16:43:34 +0100

    add useEffect exercise

---
+++ module-04-intro-to-react/Exercises/05-useEffect/src/App.jsx
@@ -0,0 +1,13 @@
+import FetchOnClick from "./components/FetchOnClick.jsx";
+import SimpleEffect from "./components/SimpleEffect.jsx";
+
+function App() {
+  return (
+    <>
+      <SimpleEffect />
+      <FetchOnClick />
+    </>
+  );
+}
+
+export default App;
