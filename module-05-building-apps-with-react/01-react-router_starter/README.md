ae37fe73eb3cf0105e4fd4e23c727b810d20c0e2
Renke Brixel <renke.brixel@gmail.com>
2025-11-26 08:17:13 +0100

    add react router starter

---
+++ module-05-building-apps-with-react/01-react-router_starter/README.md
@@ -0,0 +1,6 @@
+# React Router Example
+
+Single Page Application Routing with React Router
+
+> [!NOTE]  
+> This is an example repo from a WBS Coding School recap
---
+++ module-05-building-apps-with-react/01-react-router_starter/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Stars from "./components/Stars";
+
+function App() {
+  return (
+    <div className="body">
+      <Header />
+      <main>
+        <Stars />
+      </main>
+      <footer>© footerbla</footer>
+    </div>
+  );
+}
+
+export default App;
