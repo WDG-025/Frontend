a7b0d94ebbbbad634718f263f0116ba79a9437c6
Renke Brixel <renke.brixel@gmail.com>
2025-12-01 23:17:09 +0100

    add starter code for react 19 form exercise

---
+++ module-05-building-apps-with-react/Exercises/03-wbs-form-submission/src/pages/Register.jsx
@@ -0,0 +1,26 @@
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback, Instructions } from "../components";
+
+const Register = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <form>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-lg border p-4">
+            <legend className="fieldset-legend">Register to our newsletter</legend>
+            <label className="label" htmlFor="email">
+              Email
+            </label>
+            <input className="input w-full" name="email" placeholder="Email" id="email" />
+            <button className="btn btn-neutral mt-4" type="submit">
+              Register!
+            </button>
+          </fieldset>
+        </form>
+      </ErrorBoundary>
+      <Instructions path="/register.md" />
+    </div>
+  );
+};
+
+export default Register;
